40ad2133a20e5eebac4f3e4ed4c63cc52755fbb8
Edward Tsang <2091762t@student.gla.ac.uk>
2020-04-07 15:51:11 +0100

    Published date defaults to current date and time

--- mysite/myApp/models.py
+++ mysite/myApp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils.timezone import now
 
 class newsArticle(models.Model):
     article_heading = models.CharField(max_length=200)
     article_text = models.CharField(max_length=1024)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', default=now)
 
 # Create your models here.
